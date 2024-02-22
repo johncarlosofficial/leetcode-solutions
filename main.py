3b7f2d1c56ad69cded49c9db4ee137d3bbbf6aa2
John Carlos <johncarlosdeveloper@gmail.com>
2024-02-22 17:33:16 +0000

    feat(p0169): add solution

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from python.p0027 import Solution
+from python.p0169 import Solution
 
 solution = Solution()
-print(solution.removeElement([3,2,2,3], 3))
+print(solution.majorityElement([2,2,1,1,1,2,2]))
