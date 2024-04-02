7a7a561de281b9f193395b10b8a5e7c4ae2455db
John Carlos <johncarlosdeveloper@gmail.com>
2024-04-02 12:31:07 +0000

    feat(p0744): add solution

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from python.p0744 import Solution
 
 solution = Solution()
-print(solution.nextGreatestLetter(["x","x","y","y"], "z"))
+print(solution.nextGreatestLetter(["x", "x", "y", "y"], "z"))
--- python/p0744.py
+++ python/p0744.py
@@ -4,18 +4,18 @@ from typing import List
 class Solution:
 
   def nextGreatestLetter(self, letters: List[str], target: str) -> str:
-      left, right = 0, len(letters) - 1
-  
-      while left <= right:
-          mid = (left + right) // 2
-          mid_val = letters[mid]
-  
-          if mid_val <= target:
-              left = mid + 1
-          else:
-              right = mid - 1
-  
-      if left < len(letters):
-          return letters[left]
+    left, right = 0, len(letters) - 1
+
+    while left <= right:
+      mid = (left + right) // 2
+      mid_val = letters[mid]
+
+      if mid_val <= target:
+        left = mid + 1
       else:
-          return letters[0]
+        right = mid - 1
+
+    if left < len(letters):
+      return letters[left]
+    else:
+      return letters[0]
