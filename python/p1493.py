ca83af9129e131455be74d1eae69f8f54c6546cd
John Carlos <johncarlosdeveloper@gmail.com>
2024-05-06 11:15:58 +0000

    feat(p1493): add solution

---
+++ python/p1493.py
@@ -0,0 +1,22 @@
+from typing import List
+
+
+class Solution:
+
+    def longestSubarray(self, nums: List[int]) -> int:
+        zero_count = 0
+        left = 0
+        max_length = 0
+
+        for right in range(len(nums)):
+            if nums[right] == 0:
+                zero_count += 1
+
+            while zero_count > 1:
+                if nums[left] == 0:
+                    zero_count -= 1
+                left += 1
+
+            max_length = max(max_length, right - left)
+
+        return max_length
