a6062c2d2210f23472f50c6871178a6a8dd9731d
John Carlos <johncarlosdeveloper@gmail.com>
2024-05-21 16:51:03 +0000

    feat(p0933): add solution

--- main.py
+++ main.py
@@ -1,4 +1,27 @@
-from python.p1071 import Solution
+import queue
 
-sol = Solution()
-print(sol.gcdOfStrings("ABCABC", "ABC"))
+
+class RecentCounter:
+
+    def __init__(self):
+        self.counter = 0
+        self.queue = queue.Queue()
+
+
+    def ping(self, t: int) -> int:
+        self.queue.put(t)
+        self.counter += 1
+
+        min_val = t - 3000
+
+        while self.queue.queue[0] < min_val:
+            self.queue.get()
+            self.counter -= 1
+
+        return self.counter
+
+q = RecentCounter()
+print(q.ping(1))
+print(q.ping(100))
+print(q.ping(3001))
+print(q.ping(3002))
