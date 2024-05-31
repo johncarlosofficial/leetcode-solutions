c3334b5dfcb11dec7b578fd8d32a6a461c31e5aa
John Carlos <johncarlosdeveloper@gmail.com>
2024-05-31 13:07:26 +0000

    🟢 Solve problem 35

--- python/p0035.py
+++ python/p0035.py
@@ -4,10 +4,19 @@ from typing import List
 class Solution:
 
   def searchInsert(self, nums: List[int], target: int) -> int:
-    i = 0
-    while i < len(nums):
-      if nums[i] < target:
-        i += 1
+    left = 0
+    right = len(nums) - 1
+
+    while left <= right:
+      mid = (left + right) // 2
+
+      if nums[mid] == target:
+        return mid
+
+      elif nums[mid] < target:
+        left = mid + 1
+
       else:
-        return i
-    return i
+        right = mid - 1
+
+    return left
