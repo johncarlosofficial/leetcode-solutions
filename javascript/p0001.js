b8d9eed5043c812daf78caa7b7d83f179932509e
John Carlos <johncarlosdeveloper@gmail.com>
2024-06-10 07:33:06 -0300

    ⚰️ Remove replit files

--- javascript/p0001.js
+++ javascript/p0001.js
@@ -3,17 +3,16 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
-    hash_map = {}
+export var twoSum = function (nums, target) {
+  let hash_map = {};
 
-    for (let i = 0; i < nums.length; i++) {
-        if ((target - nums[i]) in hash_map) {
-            return [hash_map[target - nums[i]], i]
-        }
-        else {
-            hash_map[nums[i]] = i
-        }
+  for (let i = 0; i < nums.length; i++) {
+    if (target - nums[i] in hash_map) {
+      return [hash_map[target - nums[i]], i];
+    } else {
+      hash_map[nums[i]] = i;
     }
+  }
 
-    return []
+  return [];
 };
---
+++ main.js
@@ -0,0 +1,7 @@
+import { twoSum } from "./javascript/p0001.js";
+
+const nums = [2, 7, 11, 15];
+const target = 9;
+const result = twoSum(nums, target);
+
+console.log(result); // Output should be [0, 1]
