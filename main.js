6ec42c95ab469022ff7a4f426cdb77b4e9d24fe5
John Carlos <johncarlosdeveloper@gmail.com>
2024-07-27 15:01:46 -0300

    🗑️ Delete main files

--- main.js
+++
@@ -1,5 +0,0 @@
-import { evalRPN } from "./javascript/p0150.js";
-
-console.log(
-  evalRPN(["10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"])
-);
--- main.py
+++
@@ -1,5 +0,0 @@
-from python.p0150 import Solution
-
-solution = Solution()
-
-print(solution.evalRPN(["10","6","9","3","+","-11","*","/","*","17","+","5","+"]))
