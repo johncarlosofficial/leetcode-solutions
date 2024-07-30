e227880307842176c65f21cf44a3392ae48708f0
John Carlos <johncarlosdeveloper@gmail.com>
2024-07-30 06:43:24 -0300

    🟡 Solve problem 33

--- python/p0033.py
+++ python/p0033.py
@@ -4,25 +4,27 @@ from typing import List
 class Solution:
 
     def search(self, nums: List[int], target: int) -> int:
-        l = 0
-        r = len(nums) - 1
+        left, right = 0, len(nums) - 1
 
-        while l <= r:
-            m = (l + r) // 2
+        while left <= right:
+            mid = (left + right) // 2
 
-            if target == nums[m]:
-                return m
+            if nums[mid] == target:
+                return mid
 
-            if nums[m] < nums[r]:
-                if target > nums[m] and target <= nums[r]:
-                    l = m + 1
+            # If the left partition is sorted
+            if nums[mid] >= nums[left]:
+                # If the target is in the range of the left partition
+                if nums[left] <= target < nums[mid]:
+                    right = mid - 1
                 else:
-                    r = m - 1
-
+                    left = mid + 1
+            # If the right partition is sorted
             else:
-                if target < nums[m] and target >= nums[l]:
-                    r = m - 1
+                # If the target is in the range of the right partition
+                if nums[mid] < target <= nums[right]:
+                    left = mid + 1
                 else:
-                    l = m + 1
+                    right = mid - 1
 
         return -1
